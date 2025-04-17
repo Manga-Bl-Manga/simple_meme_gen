89d5689398a8108526d413472c80b6a98ccc45fd
Manga_Bl_Manga <gogo05tv@gmail.com>
2025-04-17 15:10:51 +0200

    feat(meme): add image with overlaid text
    - Uses absolute positioning for text -Supports dynamic image URLs

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,15 +1,36 @@
-export default function Body(){
-    return(
-        <>
-            <div className="form">
-                <label>top-text
-                    <input type="text" name="top-half"/>
-                </label>
+import "./styles/Body.css"
+import {useState} from 'react'
 
-                <label>bottom-text
-                    <input type="text" name="top-half"/>
-                </label>
-            </div>   
+export default function Body() {
+
+    const [memeObj,setMemeObj] = useState({
+        url:"http://i.imgflip.com/1bij.jpg",
+        topText:"One does not simply",
+        bottomText:"Walk into Mordor"
+        })
+
+    return (
+        <>
+            <div className="theMeat">
+                <div className="form">
+                    <label>Top Text
+                        <input type="text" name="top-half" />
+                    </label>
+
+                    <label>Bottom Text
+                        <input type="text" name="top-half" />
+                    </label>
+
+                </div>
+                <div className="get-image">
+                    <button>Get a new cat image!</button>
+                </div>
+                <div className="meme">
+                    <img src={memeObj.url} />
+                    <span className="top">{memeObj.topText}</span>
+                    <span className="bottom">{memeObj.bottomText}</span>
+                </div>
+            </div>
         </>
     )
 }
